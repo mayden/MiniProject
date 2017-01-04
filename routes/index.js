00a2d99d8b2cefada3c92eaab38a48b6890f5791
Alex <alex@alnet.co.il>
2017-01-04 13:25:01 +0200

    Save file to xml

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var request = require("request");
-
+var json2csv = require('json2csv');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var apiUrl = "http://oknesset.org/api/v2/bill";
+    var fieldNames = ['Title', 'URL', 'Date', 'Title', 'Popular Name', 'Stage', 'Resource URL'];
 
     request({
         url: apiUrl,
         json: true
-    }, function (error, response, body) {
+    }, function (error, response, apiData) {
         if (!error && response.statusCode === 200) {
-            console.log(body) // Print the json response
+            var csv = json2csv({ data: apiData.objects, fieldNames: fieldNames });
+            fs.writeFile('file.csv', csv, function(err) {
+                if (err) throw err;
+                console.log('File has been succesfully saved');
+            });
         }
     })
   res.render('index', {
@@ -20,3 +26,4 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
